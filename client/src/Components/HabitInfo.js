a757ddeb66d0d9bbf449cc552129491f9f390873
백현영 <baeghyeon-yeong@baeghyeon-yeong-ui-MacBookPro.local>
2020-05-21 16:23:54 +0900

    ADD HabitList,HabitInfo
      - db에서 습관정보를 받아와 리스트에 보내주는 코드 작성.
      - db로부터의 get API는 아직 안적혀있음.

---
+++ client/src/Components/HabitInfo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function HabitInfo(props) {
+  return (
+    <div>
+      <div>{props.info}</div>
+    </div>
+  );
+}
+export default HabitInfo;
--- client/src/Components/HabitList.js
+++ client/src/Components/HabitList.js
@@ -1,7 +1,47 @@
 import React from 'react';
+import HabitInfo from './HabitInfo';
 
-function HabitList(props) {
-  return <div className='HabitList'>HabitList</div>;
+class HabitList extends React.Component {
+  state = {
+    habitlist: [
+      {
+        id: 0,
+        habitName: 'test',
+      },
+    ],
+  };
+  componentDidMount() {
+    fetch(
+      'http://localhost:4000/habit',
+      // 'http://54.180.103.96:4000/habit',
+      {
+        method: 'GET',
+        withCredentials: true,
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    )
+      .then((result) => {
+        return result.json();
+      })
+      .then((data) => {
+        //
+      });
+  }
+
+  render() {
+    const list = this.state.habitlist.map((habit) => {
+      return <HabitInfo key={habit.id} info={habit.habitName} />;
+    });
+    return (
+      <div className="HabitList">
+        <div>{list}</div>
+        <button className="AddHabit"> + </button>
+      </div>
+    );
+  }
 }
 
 export default HabitList;
