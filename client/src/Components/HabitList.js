b9ac29009693c2cd1c71bebba30e092be0e4d0bc
keuni <kyungeun.kiim@gmail.com>
2020-05-21 18:56:38 +0900

    ADD main calendar
     - 달력 그리고 css 수정,
       성공률 따라 색깔 표시하였고,
       성공률 가져오는 건 아직 미완료

--- client/src/Components/HabitList.js
+++ client/src/Components/HabitList.js
@@ -1,7 +1,45 @@
 import React from 'react';
 
-function HabitList(props) {
-  return <div className='HabitList'>HabitList</div>;
+class HabitList extends React.Component {
+  state = {
+    newHabit: '',
+    addHabit: false,
+  };
+
+  handleInputValue = (e) => {
+    this.setState({ newHabit: e.target.value });
+  };
+
+  addHabit() {
+    this.setState({
+      addHabit: !this.state.addHabit,
+    });
+  }
+
+  render() {
+    return (
+      <div className='HabitList'>
+        HabitList
+        <div>
+          {this.state.addHabit ? (
+            <div>
+              <input
+                type='text'
+                placeholder='습관을 추가하세요'
+                onChange={this.handleInputValue.bind(this)}
+              ></input>
+              <button className='btnAdd' onClick={this.addHabit.bind(this)}>
+                Add
+              </button>
+            </div>
+          ) : (
+            ''
+          )}
+          <button onClick={this.addHabit.bind(this)}>+</button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default HabitList;
