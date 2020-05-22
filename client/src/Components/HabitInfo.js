10e43a04930a55f51aafd083f3be34c0ae34f34b
백현영 <baeghyeon-yeong@baeghyeon-yeong-ui-MacBookPro.local>
2020-05-22 15:31:39 +0900

    ADD Habit checkBox
      - 습관완료시 사용할 체크 기능 추가

--- client/src/Components/HabitInfo.js
+++ client/src/Components/HabitInfo.js
@@ -1,6 +1,18 @@
 import React from 'react';
 
-function HabitInfo(props) {
-  return <div className='existingHabit'>{props.info}</div>;
+class HabitInfo extends React.Component {
+  onToggle = () => {
+    this.props.recordComplete(this.props.id);
+  };
+  render() {
+    return (
+      <div>
+        <div className="existingHabit">
+          {this.props.info}
+          <input type="checkbox" onClick={this.onToggle.bind(this)}></input>
+        </div>
+      </div>
+    );
+  }
 }
 export default HabitInfo;
